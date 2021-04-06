1875b7253189936c091bddf808e9fb9dd5b694e5
Ben <shilinchen1010@gmail.com>
2021-04-06 02:04:33 -0700

    clean data from fetching data

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ require('./routes/cucktailRoutes')(app);
 const PORT = 5000;
 
 app.listen(PORT, () => {
-  console.log(`Listening on PORT ${PORT}`);
+  console.log(`Listening on port: ${PORT} ...`);
 });
--- routes/cucktailRoutes.js
+++ routes/cucktailRoutes.js
@@ -18,22 +18,37 @@ async function grabTenCocktails() {
   return cucktails;
 }
 
-//grabTenCocktails();
+function getCuckTailData(data) {
+  cucktails = {};
+  cucktails['name'] = data['strDrink'];
+  cucktails['instruction'] = data['strInstructions'];
+  cucktails['imageURL'] = data['strDrinkThumb'];
+  cucktails['ingredient'] = [];
+  cucktails['measure'] = [];
+  for (let i = 0; i < 15; i++) {
+    if (data[`strIngredient${i}`] != null) {
+      cucktails['ingredient'].push(data[`strIngredient${i}`]);
+      cucktails['measure'].push(data[`strMeasure${i}`]);
+    }
+  }
+  return cucktails;
+}
+
 module.exports = (app) => {
   app.get('/', async (req, res) => {
     try {
       const randomCocktail = await axios.get(
         'https://www.thecocktaildb.com/api/json/v1/1/random.php'
       );
+
       res.send(randomCocktail.data);
-      console.log(randomCocktail.data);
+      const cucktail = getCuckTailData(randomCocktail.data['drinks'][0]);
+      console.log(randomCocktail.data['drinks'][0]);
+      console.log(cucktail);
+      //console.log(data);
     } catch (err) {
       console.log('Error', err);
       res.status(500).end(err.message);
     }
   });
-
-  //   app.listen(5000, () => {
-  //     console.log(`listening on port: ${port}`);
-  //   });
 };
