6e658ad6a2afafd4de7f309c6cda16c91e5c65a7
Ben <shilinchen1010@gmail.com>
2021-04-16 03:21:56 -0700

    add footer

---
+++ client/src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Navbar, Container, NavbarBrand } from 'react-bootstrap';
+import LocalBarIcon from '@material-ui/icons/LocalBar';
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <Navbar bg="dark" variant="dark">
+        <Container className="myNavbar justify-content-center">
+          <NavbarBrand>
+            <LocalBarIcon color="inherit" fontSize="large" /> MyBestBar
+          </NavbarBrand>
+        </Container>
+      </Navbar>
+    </div>
+  );
+}
