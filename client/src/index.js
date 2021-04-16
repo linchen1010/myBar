f3e3506e0edcc3387ecebefe025e1e6bba2fefd9
Ben <shilinchen1010@gmail.com>
2021-04-16 02:17:55 -0700

    add back restrict mode

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,12 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
