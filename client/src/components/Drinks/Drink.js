ee7ba247872305250b391dc5ccfac3a1bb410125
Ben <shilinchen1010@gmail.com>
2021-04-23 16:52:19 -0700

    edit drinks & drinks component and some styling

--- client/src/components/Drinks/Drink.js
+++ client/src/components/Drinks/Drink.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { useParams } from 'react-router-dom';
 
-export default function Drink() {
-    return (
-        <div>
-            <a href="/drinks/category" className="href">category</a>
-        </div>
-    )
+export default function Drink({ category }) {
+  return (
+    <div>
+      <a href={`/drinks/${category}`}>
+        <img
+          src="https://images.unsplash.com/photo-1509404969887-525aab7cc41a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1100&q=80"
+          alt="cocktail"
+          className="categoryImg"
+        ></img>
+      </a>
+      <div className="cocktailName">{category}</div>
+    </div>
+  );
 }
--- client/src/components/Drinks/Drinks.js
+++ client/src/components/Drinks/Drinks.js
@@ -4,18 +4,41 @@ import Drink from './Drink';
 export default function Drinks() {
   return (
     <div>
-      <Container>
+      <Container fluid="sm">
         <div className="cocktailDetailName">Drinks</div>
-        <Row className="m-auto">
+        <Row className="justify-content-md-center">
           <Col>
             <div className="detailTitle">
               we offer different categories of drinks for you
             </div>
           </Col>
         </Row>
-        <Row>
-        <Col>
-          <Drink/>
+        <Row className="justify-content-center" style={{ textAlign: 'center' }}>
+          <Col>
+            <Drink category="Ordinary Drink" />
+          </Col>
+          <Col>
+            <Drink category="Cocktail" />
+          </Col>
+          <Col>
+            <Drink category="Shot" />
+          </Col>
+          <Col>
+            <Drink category="Punch / Party Drink" />
+          </Col>
+        </Row>
+        <Row className="justify-content-center" style={{ textAlign: 'center' }}>
+          <Col>
+            <Drink category="Homemade Liqueur" />
+          </Col>
+          <Col>
+            <Drink category="Punch / Party Drink" />
+          </Col>
+          <Col>
+            <Drink category="Coffee / Tea" />
+          </Col>
+          <Col>
+            <Drink category="Soft Drink / Soda" />
           </Col>
         </Row>
       </Container>
