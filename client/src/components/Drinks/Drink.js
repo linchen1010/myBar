9b966cb967a9f299087c595e8d454c0243b24391
Ben <shilinchen1010@gmail.com>
2021-04-26 03:13:46 -0700

    fix some center aligning content

--- client/src/components/Drinks/Drink.js
+++ client/src/components/Drinks/Drink.js
@@ -5,18 +5,27 @@ import { Row, Col, Container } from 'react-bootstrap';
 export default function Drink() {
   let { category } = useParams();
   return (
-    <div style={{ marginTop: '40px' }}>
-      <div className="cocktailDetailName">{category}</div>
-
-      <div className="categoryDescription">
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book.
-      </div>
-      {/* <hr className="divider"></hr> */}
-      <Cocktails url={`/api/cocktails/drinks/${category}`} />
-      {/* <hr className="divider"></hr> */}
+    <div>
+      <Container>
+        <Row className="justify-content-center">
+          <Col>
+            <div style={{ marginTop: '40px' }} className="cocktailDetailName">
+              {category}
+            </div>
+          </Col>
+        </Row>
+        <Row className="justify-content-center">
+          <div className="categoryDescription">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book.
+          </div>
+        </Row>
+        {/* <hr className="divider"></hr> */}
+        <Cocktails url={`/api/cocktails/drinks/${category}`} />
+        {/* <hr className="divider"></hr> */}
+      </Container>
     </div>
   );
 }
