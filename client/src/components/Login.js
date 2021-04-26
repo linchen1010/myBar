66996a1ae619000b8ff564318fa185b5bb6f2318
Ben <shilinchen1010@gmail.com>
2021-04-26 02:35:57 -0700

    Login component done

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,9 +1,35 @@
 import React from 'react';
-
+import { Container, Form, Button, Row } from 'react-bootstrap';
 export default function Login() {
   return (
     <div>
-      <h1>Login Page!</h1>
+      <Container fluid="sm">
+        <Row className="cocktailCategory justify-content-center">Log In</Row>
+        <Form.Group className="loginForm">
+          <Form>
+            <Form.Control
+              className="loginForm"
+              type="username"
+              placeholder="Username"
+              size="lg"
+            />
+            <Form.Control
+              className="loginForm"
+              type="password"
+              placeholder="Password"
+              size="lg"
+            />
+            <Button variant="success" bsPrefix="btn-default">
+              Login
+            </Button>
+            <Row className="justify-content-center">
+              <div className="formText">
+                Do not have an account yet? <a href="/signup">Sign up here!</a>
+              </div>
+            </Row>
+          </Form>
+        </Form.Group>
+      </Container>
     </div>
   );
 }
