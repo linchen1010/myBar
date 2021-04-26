c3c95388fc8a0703150171f1ce4490d6eebc1a32
Ben <shilinchen1010@gmail.com>
2021-04-26 02:37:14 -0700

    edit form & button & footer styling

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import { Navbar, Container, NavbarBrand } from 'react-bootstrap';
+import { Navbar, Container, NavbarBrand, Nav } from 'react-bootstrap';
 import LocalBarIcon from '@material-ui/icons/LocalBar';
 
 export default function Footer() {
+  const style = {
+    color: 'white',
+  };
   return (
     <div className="footer">
       <Navbar bg="dark" variant="dark">
-        <Container className="myNavbar justify-content-center">
-          <NavbarBrand href="/" to="/">
+        <Container className="justify-content-center myNavbar">
+          <Nav.Link href="/" to="/" style={style}>
             <LocalBarIcon color="inherit" fontSize="large" /> MyBestBar
-          </NavbarBrand>
+          </Nav.Link>
+          <Nav style={style}> © 2021 </Nav>
         </Container>
       </Navbar>
     </div>
