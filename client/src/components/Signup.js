2683038ccfd2025c9fe7d72c3a391da123a250ed
Ben <shilinchen1010@gmail.com>
2021-04-26 02:36:20 -0700

    signup component done

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,9 +1,37 @@
 import React from 'react';
-
+import { Container, Form, Button, Row } from 'react-bootstrap';
 export default function Signup() {
   return (
     <div>
-      <h1>Sign up page!</h1>
+      <Container fluid="sm">
+        <Row className="cocktailCategory justify-content-center">
+          Start your cocktails journey.
+        </Row>
+        <Form.Group className="loginForm">
+          <Form>
+            <Form.Control
+              className="loginForm"
+              type="username"
+              placeholder="Username"
+              size="lg"
+            />
+            <Form.Control
+              className="loginForm"
+              type="password"
+              placeholder="Password"
+              size="lg"
+            />
+            <Button variant="success" bsPrefix="btn-default">
+              Create account
+            </Button>
+            <Row className="justify-content-center">
+              <div className="formText">
+                Already have an account? <a href="/login">Log in</a>
+              </div>
+            </Row>
+          </Form>
+        </Form.Group>
+      </Container>
     </div>
   );
 }
