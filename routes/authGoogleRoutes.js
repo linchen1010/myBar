06305f86dd9debe14597c98b88dac041a8fd28a4
Ben <shilinchen1010@gmail.com>
2021-04-28 02:27:41 -0700

    edit typo on routes

--- routes/authGoogleRoutes.js
+++ routes/authGoogleRoutes.js
@@ -58,12 +58,12 @@ module.exports = (app) => {
     }
   );
 
-  app.get('api/logout', (req, res) => {
+  app.get('/api/logout', (req, res) => {
     req.logout();
     res.redirect('/');
   });
 
-  app.get('api/current_user', (req, res) => {
+  app.get('/api/current_user', (req, res) => {
     res.send(req.user);
   });
 };
