085a30f1e83cd8d74579d9f4245a1ce2b2f2a809
Ben <shilinchen1010@gmail.com>
2021-04-27 22:33:44 -0700

    add auth/google proxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,10 +2,10 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
   app.use(
-    '/api',
+    ['/api', '/auth/google'],
     createProxyMiddleware({
       target: 'http://localhost:5000',
-      changeOrigin: true,
+      // changeOrigin: true,
     })
   );
 };
