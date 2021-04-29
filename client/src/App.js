6eb2b207170c97d7b150e0de63193018f7c9f6f7
Ben <shilinchen1010@gmail.com>
2021-04-28 23:15:23 -0700

    add users status on frontend and toggle log in/out on header

--- client/src/App.js
+++ client/src/App.js
@@ -13,26 +13,47 @@ import Drinks from './components/Drinks/Drinks';
 import Drink from './components/Drinks/Drink';
 
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React, { useState, useMemo, useEffect } from 'react';
+import { UserContext } from './contexts/UserContext';
+import axios from 'axios';
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  const userInfo = useMemo(() => ({ user, setUser }), [user, setUser]);
+
+  const fetchUser = async () => {
+    const currentUser = await axios.get('/api/current_user');
+    setUser(currentUser.data);
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
     <Router>
-      <div className="Site">
-        <Header />
-        <div className="Site-content">
-          <Route exact path="/" exact component={HomeCover} />
-          <Route exact path="/" exact component={CocktailHome} />
-          <Route exact path="/drinks" component={Drinks} />
-          <Route path="/drinks/category/:category" component={Drink} />
-          <Route path="/about" component={About} />
-          <Route path="/login" component={Login} />
-          <Route path="/signup" component={Signup} />
-          <Route exact path="/cocktails/:id" component={CocktailDetail} />
-          <Route path="/ingredients/:name" component={IngredientDetail} />
-          <Route path="/search" component={CocktailSearch} />
+      <UserContext.Provider value={userInfo}>
+        <div className="Site">
+          <Header />
+          <div className="Site-content">
+            <Route exact path="/" exact component={HomeCover} />
+            <pre style={{ textAlign: 'center' }}>
+              {JSON.stringify(user, null, 2)}
+            </pre>
+            <Route exact path="/" exact component={CocktailHome} />
+            <Route exact path="/drinks" component={Drinks} />
+            <Route path="/drinks/category/:category" component={Drink} />
+            <Route path="/about" component={About} />
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={Signup} />
+            <Route exact path="/cocktails/:id" component={CocktailDetail} />
+            <Route path="/ingredients/:name" component={IngredientDetail} />
+            <Route path="/search" component={CocktailSearch} />
+          </div>
+          <Route path="/" component={Footer} />
         </div>
-        <Route path="/" component={Footer} />
-      </div>
+      </UserContext.Provider>
     </Router>
   );
 }
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,16 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useContext } from 'react';
 import { Navbar, Nav, Form, Button, FormControl } from 'react-bootstrap';
 import LocalBarIcon from '@material-ui/icons/LocalBar';
 import SearchIcon from '@material-ui/icons/Search';
-import { Link, useHistory } from 'react-router-dom';
+import { Link, Redirect, useHistory } from 'react-router-dom';
+import { UserContext } from '../contexts/UserContext';
+import axios from 'axios';
 
 export default function Header() {
   const [search, setSearch] = useState('');
   const searchEl = useRef(null);
   const history = useHistory();
+  const { user, setUser } = useContext(UserContext);
 
   const handleChange = (e) => {
     setSearch(e.target.value);
@@ -77,19 +80,43 @@ export default function Header() {
               <SearchIcon />
             </Button>
           </Form>
-
-          <Nav className="ml">
-            <Nav.Link as={Link} to="/login">
-              Log in
-            </Nav.Link>
-          </Nav>
-          <Nav className="ml">
-            <Nav.Link as={Link} to="/signup">
-              Sign up
-            </Nav.Link>
-          </Nav>
+          {user ? <Logout /> : <LoginSignUp />}
         </Navbar.Collapse>
       </Navbar>
     </div>
   );
 }
+
+// when user hasn't log in -- show Login/Signup for the users
+const LoginSignUp = () => {
+  return (
+    <div>
+      <Nav className="ml">
+        <Nav.Link as={Link} to="/login">
+          Log in
+        </Nav.Link>
+        <Nav.Link as={Link} to="/signup">
+          Sign up
+        </Nav.Link>
+      </Nav>
+    </div>
+  );
+};
+
+// when user sign in -- show logout element and log out the user
+const Logout = () => {
+  const { user, setUser } = useContext(UserContext);
+  const logoutUser = async () => {
+    await axios.get('/api/logout'); // ask server to logout user
+    await setTimeout(() => setUser(null), 1200); // set the frontend user data to null
+  };
+  return (
+    <div>
+      <Nav className="ml">
+        <Nav.Link as={Link} to="/" onClick={() => logoutUser()}>
+          Log out
+        </Nav.Link>
+      </Nav>
+    </div>
+  );
+};
