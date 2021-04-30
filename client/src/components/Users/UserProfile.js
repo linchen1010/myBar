5da42ec36e6d7a416748f375ec5ce5ca87192d67
Ben <shilinchen1010@gmail.com>
2021-04-30 14:42:11 -0700

    edit userprofile component

--- client/src/components/Users/UserProfile.js
+++ client/src/components/Users/UserProfile.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { UserContext } from '../../contexts/UserContext';
+import { Spinner, Container, Row, Col, Button } from 'react-bootstrap';
 
 export default function UserProfile() {
-    return (
-        <div>
-            User Profile!
-        </div>
-    )
+  const { user } = useContext(UserContext);
+  if (!user) return <Spinner />;
+  return (
+    <div>
+      <Container fluid="sm">
+        <Row className="justify-content-center">
+          <div className="userProfileTitle">{user.name}'s Profile</div>
+        </Row>
+        <Row className="justify-content-center">
+          <img
+            src="https://www.thecocktaildb.com/images/media/drink/vrwquq1478252802.jpg"
+            alt="profile img"
+            className="userProfileImg"
+          ></img>
+        </Row>
+        <Row className="justify-content-center">
+          <div className="userProfile">_id: {user._id}</div>
+        </Row>
+        <Row className="justify-content-center">
+          <div className="userProfile">user name: {user.name}</div>
+        </Row>
+        <Row className="justify-content-center">
+          <div className="userProfile">email address: {user.email}</div>
+        </Row>
+        <Row className="justify-content-center">
+          <Col sm="auto">
+            <Button variant="outline-success" bsPrefix="btn-form">
+              Edit Profile
+            </Button>
+          </Col>
+          <Col sm="auto">
+            <Button variant="outline-success" bsPrefix="btn-form">
+              My Favorite List
+            </Button>
+          </Col>
+          <Col sm="auto">
+            <Button variant="outline-success" bsPrefix="btn-form">
+              My Post
+            </Button>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
