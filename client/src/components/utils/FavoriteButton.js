17d6ca212634c3f456ad044d33ed8823180924e5
Ben <shilinchen1010@gmail.com>
2021-05-01 04:23:42 -0700

    add like button styling and like component

---
+++ client/src/components/utils/FavoriteButton.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+
+export default function FavoriteButton({ like }) {
+  return (
+    <div>
+      {like ? (
+        <div>
+          <FavoriteIcon style={{ fontSize: '35px' }} />
+        </div>
+      ) : (
+        <div>
+          <FavoriteBorderIcon style={{ fontSize: '35px' }} />
+        </div>
+      )}
+    </div>
+  );
+}
