bcafd7ef2e29e66a7935153446cd1af8cccd360d
Ben <shilinchen1010@gmail.com>
2021-05-08 16:43:41 -0700

    edit userPost component

--- client/src/components/Users/UserPost.js
+++ client/src/components/Users/UserPost.js
@@ -1,10 +1,26 @@
 import React, { useContext } from 'react';
 import { UserContext } from '../../contexts/UserContext';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 
 export default function UserPost() {
+  const { user } = useContext(UserContext);
   return (
     <div>
-      <h1>User post</h1>
+      <Container>
+        <Row className="justify-content-center">
+          <div className="userProfileTitle">My Posts</div>
+        </Row>
+
+        <Row className="justify-content-center">
+          {user ? (
+            <a href={`/user/${user._id}/post/new`}>
+              <Button bsPrefix="btn-random">add new post</Button>
+            </a>
+          ) : (
+            <div></div>
+          )}
+        </Row>
+      </Container>
     </div>
   );
 }
