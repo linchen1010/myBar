eb0589604e72a2a19e9f381e3573eb12465cbb30
Ben <shilinchen1010@gmail.com>
2021-05-09 04:35:05 -0700

    clean up routes in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,12 @@
 import './App.css';
 import Header from './components/Header';
-import About from './components/About';
-import Login from './components/Login';
-import Signup from './components/Signup';
-import CocktailHome from './components/Cocktails/CocktailHome';
-import CocktailDetail from './components/Cocktails/CocktailDetail';
-import CocktailSearch from './components/Cocktails/CocktailSearch';
-import IngredientDetail from './components/Ingredients/IngredientDetail';
+import MyRoutes from './MyRoutes';
 import Footer from './components/Footer';
-import Drinks from './components/Drinks/Drinks';
-import Drink from './components/Drinks/Drink';
 
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import React, { useState, useMemo, useEffect } from 'react';
 import { UserContext } from './contexts/UserContext';
 import axios from 'axios';
-import UserProfile from './components/Users/UserProfile';
-import UserFavoriteList from './components/Users/UserFavoriteList';
-import UserPost from './components/Users/UserPost';
-import PostNew from './components/Posts/PostNew';
 
 function App() {
   const [user, setUser] = useState(null);
@@ -40,22 +28,9 @@ function App() {
         <div className="Site">
           <Header />
           <div className="Site-content">
-            <Route exact path="/" exact component={CocktailHome} />
-            {/* <pre>{JSON.stringify(user, null, 2)}</pre> */}
-            <Route exact path="/drinks" component={Drinks} />
-            <Route path="/drinks/category/:category" component={Drink} />
-            <Route path="/about" component={About} />
-            <Route path="/login" component={Login} />
-            <Route path="/signup" component={Signup} />
-            <Route exact path="/cocktails/:id" component={CocktailDetail} />
-            <Route path="/ingredients/:name" component={IngredientDetail} />
-            <Route path="/search" component={CocktailSearch} />
-            <Route exact path="/user" component={UserProfile} />
-            <Route path="/user/favorite" component={UserFavoriteList} />
-            <Route exact path="/user/post" component={UserPost} />
-            <Route path="/user/post/new" component={PostNew} />
+            <MyRoutes />
           </div>
-          <Route path="/" component={Footer} />
+          <Footer />
         </div>
       </UserContext.Provider>
     </Router>
---
+++ client/src/MyRoutes.js
@@ -0,0 +1,42 @@
+import About from './components/About';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import CocktailHome from './components/Cocktails/CocktailHome';
+import CocktailDetail from './components/Cocktails/CocktailDetail';
+import CocktailSearch from './components/Cocktails/CocktailSearch';
+import IngredientDetail from './components/Ingredients/IngredientDetail';
+
+import Drinks from './components/Drinks/Drinks';
+import Drink from './components/Drinks/Drink';
+
+import { Route, Switch } from 'react-router-dom';
+import React from 'react';
+
+import UserProfile from './components/Users/UserProfile';
+import UserFavoriteList from './components/Users/UserFavoriteList';
+import UserPost from './components/Users/UserPost';
+import PostNew from './components/Posts/PostNew';
+import Post from './components/Posts/Post';
+
+export default function MyRoutes() {
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" exact component={CocktailHome} />
+        <Route exact path="/drinks" component={Drinks} />
+        <Route path="/drinks/category/:category" component={Drink} />
+        <Route path="/about" component={About} />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
+        <Route exact path="/cocktails/:id" component={CocktailDetail} />
+        <Route path="/ingredients/:name" component={IngredientDetail} />
+        <Route path="/search" component={CocktailSearch} />
+        <Route exact path="/user" component={UserProfile} />
+        <Route path="/user/favorite" component={UserFavoriteList} />
+        <Route exact path="/user/posts" component={UserPost} />
+        <Route exact path="/user/posts/new" component={PostNew} />
+        <Route exact path="/user/posts/:postId" component={Post} />
+      </Switch>
+    </div>
+  );
+}
