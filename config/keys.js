ee5fd6ea6041f9a012ef5b44456ee01632d85557
Ben <shilinchen1010@gmail.com>
2021-05-09 22:15:48 -0700

    add keys and gitignore

--- config/keys.js
+++ config/keys.js
@@ -1,13 +1,7 @@
-module.exports = {
-  googleClientID:
-    '859303563378-blb393c1cagl2me7puh1v47sk4bbg81t.apps.googleusercontent.com',
-  googleClientSecret: 'tcmahrwPfAjDzOSnV-_h-7V9',
-  mongoURI:
-    'mongodb+srv://dbmybar:Fi1lMhgNfJ507WoC@cluster0.bayf5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-  mongoURI2:
-    'mongodb+srv://mbar:5jjHls3OSV2ldQHL@cluster0.s2xci.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-  mongoURI3:
-    'mongodb+srv://Timmy:1HwE6Sha1RB0N7QO@cluster0.jb1ay.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-  cookieKey: 'qwejzmxcopqiwemasdoyrelwqmzxcloawer',
-  SESSION_SECRET: 'secret',
-};
+if (process.env.NODE_ENV === 'production') {
+  // production
+  module.exports = require('./prod');
+} else {
+  // development
+  module.exports = require('./dev');
+}
